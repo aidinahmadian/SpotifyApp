ed292223371d465536ea05a212c8c8bf0284f68e
Aidin <ahmadian_aidin@yahoo.com>
2021-04-07 17:00:32 +0430

    Library UI

---
+++ Spotify App/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify App
+//
+//  Created by Aidin Ahmadian on 4/7/21.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ Spotify App/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify App
+//
+//  Created by Aidin Ahmadian on 4/7/21.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
